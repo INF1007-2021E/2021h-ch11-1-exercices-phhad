43821777533d18f3c934426843f446fa8353c968
phhad <philippe.hadjis@polymtl.ca>
2021-03-31 19:26:39 -0400

    Update game.py

--- game.py
+++ game.py
@@ -17,8 +17,39 @@ class Weapon:
 	:param power: Le niveau d'attaque
 	:param min_level: Le niveau minimal pour l'utiliser
 	"""
-	def __init__(self, name,
 	UNARMED_POWER = 20
+
+	def __init__(self, name, power, min_level):
+		self.__name = name
+		self.power = power
+		self.min_level = min_level
+
+
+
+	@property
+	def min_level(self):
+		return self.min_level
+
+	@min_level.setter
+	def min_level(self, value):
+		self.min_level = value
+
+	@property
+	def power(self):
+		return self.power
+
+	@power.setter
+	def min_level(self, value):
+		self.power = value
+
+	@property
+	def name(self):
+		return self.__name
+
+	@staticmethod
+	def make_unarmed(self):
+		return Weapon('unarmed', self.UNARMED_POWER, 1)
+
 
 
 class Character:
@@ -31,6 +62,18 @@ class Character:
 	:param defense: Le niveau de défense du personnage
 	:param level: Le niveau d'expérience du personnage
 	"""
+
+	def __init__(self, name, max_hp, attack, defense, level):
+		self.name = name
+		self.max_hp = max_hp
+		self.attack = attack
+		self.defense = defense
+		self.level = level
+
+	def compute_damage(self,defendeur):
+
+		pass
+
 
 
 def deal_damage(attacker, defender):
